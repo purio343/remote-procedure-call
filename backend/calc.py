2f2b447737176349370490576832bef9fb2efcdd
purio <wizard.bass5@gmail.com>
2025-03-30 19:16:32 +0900

    サーバー側のコメントを追記

--- backend/calc.py
+++ backend/calc.py
@@ -1,6 +1,7 @@
 from func_list import *
 
 def calc(func_name, params):
+    # 関数の実行結果を辞書に格納するのではなく、関数をそのまま格納する。
     func_table = {
         "floor": floor,
         "nroot": nroot,
@@ -9,7 +10,6 @@ def calc(func_name, params):
         "sort": sort
     }
 
-    # return func_table[func_name]
     if func_name in func_table:
         return func_table[func_name](params)
     else:
