30d91ca7c8830f92c43cedec82953dae9d4f2e7f
purio <wizard.bass5@gmail.com>
2025-04-03 11:45:26 +0900

    ソケット作成処理を関数化。

--- backend/server.py
+++ backend/server.py
@@ -10,15 +10,13 @@ def run_server():
     cleanup_socket()
 
     # ソケット作成
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    sock.bind(server_address)
-    sock.listen(5)
-    print(f'Server starting up on {server_address}')
+    sock = create_socket(server_address)
 
     try:
         while True:
             connection, address = sock.accept()
             print('New connection established')
+            # データの受信とその処理
             handle_client(connection)
     except Exception as e:
         print(f'An error occured: {e}')
@@ -53,6 +51,14 @@ def handle_client(connection):
     finally:
         print('Closing connection')
         connection.close()
+
+def create_socket(address):
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.bind(address)
+    sock.listen(5)
+    print(f'Server starting up on {address}')
+
+    return sock
 
 if __name__ == "__main__":
     run_server()
